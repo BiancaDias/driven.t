935d2502c433cbcda56978988d43fb18380ad116
Bianca Dias <bianca.dias@ufv.br>
2023-06-30 17:16:26 -0300

    mudanças de tratamento de erro 3

--- src/services/enrollments-service/index.ts
+++ src/services/enrollments-service/index.ts
@@ -11,7 +11,7 @@ async function getAddressFromCEP(cep: string) {
   // FIXME: está com CEP fixo!
   const result = await request.get(`${process.env.VIA_CEP_API}/${cep}/json/`);
 
-  if (!result.data) {
+  if (!result.data || result.data.error === true) {
     throw notFoundError();
   }
 
