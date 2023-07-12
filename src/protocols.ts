a0a6cfd8b57a8500fd8b1ef99f426f507550eca8
Bianca Dias <bianca.dias@ufv.br>
2023-07-12 11:16:36 -0300

    merge

--- src/protocols.ts
+++ src/protocols.ts
@@ -1,5 +1,6 @@
 import { TicketStatus } from "@prisma/client";
 
+
 export type ApplicationError = {
   name: string;
   message: string;
@@ -57,3 +58,24 @@ export type Payment = {
     cvv: number;
   };
 };
+
+
+export type Tick = { ticketTypeId: number };
+
+export type TicketFormat = {
+  id: number,
+  status: TicketStatus,
+  ticketTypeId: number,
+  enrollmentId: number,
+  TicketType: {
+    id: number,
+    name: string,
+    price: number,
+    isRemote: boolean,
+    includesHotel: boolean,
+    createdAt: Date,
+    updatedAt: Date,
+  },
+  createdAt: Date,
+  updatedAt: Date,
+}
